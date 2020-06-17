baaf45565b6e44acbcc73e38f9a1d64f100836f9
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-17 23:28:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,7 +47,7 @@ def hit?(player_total)
    if user_input != 'h' && user_input != 's'
     invalid_command
     prompt_user
-    
+    user_input = get_user_input
     puts "Please enter a valid command"
   end
   if user_input == 's'
