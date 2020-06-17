a5f21b3b96f5a3d3710023ceb795c9e925ea2d93
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-17 20:07:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,21 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table."
 end
 
 def deal_card
   # code #deal_card here
+  dealt_card = rand(1..11)
+  puts dealt_card
 end
 
 def display_card_total
   # code #display_card_total here
+  deal_card
+  cards_in_hand = []
+  cards_in_hand.push(deal_card)
+  card_total += deal_card
+  
 end
 
 def prompt_user
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,25 @@
-require_relative "blackjack.rb"
+# require_relative "blackjack.rb"
 
-runner
+# runner
+
+def deal_card
+  # code #deal_card here
+  dealt_card = rand(1..11)
+  puts dealt_card
+end
+
+def display_card_total
+  # code #display_card_total here
+  puts "deal card methond used #{deal_card}"
+  cards_in_hand = []
+  p cards_in_hand.push(deal_card)
+  card_total = 0
+  puts "card total is #{card_total += deal_card.to_i}"
+  
+end
+
+display_card_total
+
+
+
+
