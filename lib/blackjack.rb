e37bc06c03eea9153718d6a9c15e35d5551622ef
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-17 23:34:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -78,5 +78,15 @@ def runner
 #     display_card_total(total)
 #   end
 # end_game(total)
+welcome
+   = initial_round
+  card_total = user_hand
+
+  until card_total >= 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
+end
 end
     
