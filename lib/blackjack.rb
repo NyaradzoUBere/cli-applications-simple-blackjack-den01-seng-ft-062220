3eefaf37113a9fdb4edbe1da0a69efe94b50016d
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-17 23:41:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -75,7 +75,7 @@ def runner
 
   until total >= 21
     total = hit?(total)
-    puts "Your cards add up to #{display_card_total(total)})
+    puts "Your cards add up to #{display_card_total(total)}"
   end
   end_game(total)
 end
